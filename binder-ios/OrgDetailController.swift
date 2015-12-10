56c20f3288d4d414c1a60d6518ca0f3f5d907312
Aamer Rakla <arakla@AAMER-MAC.WV.CC.CMU.EDU>
2015-12-10 18:20:52 -0500

    Updated OrgDetailController;

--- binder-ios/OrgDetailController.swift
+++ binder-ios/OrgDetailController.swift
@@ -25,7 +25,7 @@ class OrgDetailController: UIViewController {
     @IBOutlet weak var statusLabel: UILabel!
     @IBOutlet weak var chair1Label: UILabel!
     @IBOutlet weak var phone1Label: UILabel!
-    @IBOutlet weak var chairwLabel: UILabel!
+    @IBOutlet weak var chair2Label: UILabel!
     @IBOutlet weak var phone2Label: UILabel!
     @IBOutlet weak var chair3Label: UILabel!
     @IBOutlet weak var phone3Label: UILabel!
@@ -46,8 +46,23 @@ class OrgDetailController: UIViewController {
             if let status = self.statusLabel {
                 status.text = detail.status
             }
+            if let chair1 = self.chair1Label {
+                chair1.text = detail.chair1
+            }
             if let phone1 = self.phone1Label {
                 phone1.text = detail.phone1
+            }
+            if let chair2 = self.chair2Label {
+                chair2.text = detail.chair2
+            }
+            if let phone2 = self.phone2Label {
+                phone2.text = detail.phone2
+            }
+            if let chair3 = self.chair3Label {
+                chair3.text = detail.chair3
+            }
+            if let phone3 = self.phone3Label {
+                phone3.text = detail.phone3
             }
         }
     }
