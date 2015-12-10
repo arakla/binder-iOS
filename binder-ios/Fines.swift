8d35166f367d3437a880b5c84946391e21b6737a
Aamer Rakla <arakla@AAMER-MAC.WV.CC.CMU.EDU>
2015-12-10 12:54:04 -0500

    Modified to reflect scope

--- binder-ios/Fines.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Fines.swift
-//  binder-ios
-//
-//  Created by Aamer Rakla on 12/7/15.
-//  Copyright (c) 2015 SCC. All rights reserved.
-//
-
-import Foundation
--- binder-ios/OrgDetailController.swift
+++ binder-ios/OrgDetailController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class FinesDetailController: UIViewController {
+class OrgDetailController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
