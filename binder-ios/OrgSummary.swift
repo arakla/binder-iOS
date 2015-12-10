050b7ac852d5a803e9c21b704a04a5f3a5d07ae8
Aamer Rakla <arakla@AAMER-MAC.WV.CC.CMU.EDU>
2015-12-10 17:57:56 -0500

    Rename OrgSummary to Org, made org struct;

--- binder-ios/OrgSummary.swift
+++
@@ -1,9 +0,0 @@
-//
-//  OrgSummary.swift
-//  binder-ios
-//
-//  Created by Aamer Rakla on 12/7/15.
-//  Copyright (c) 2015 SCC. All rights reserved.
-//
-
-import Foundation
---
+++ binder-ios/Organization.swift
@@ -0,0 +1,31 @@
+//
+//  OrgSummary.swift
+//  binder-ios
+//
+//  Created by Aamer Rakla on 12/7/15.
+//  Copyright (c) 2015 SCC. All rights reserved.
+//
+
+import Foundation
+
+struct Organization {
+    var name: String?
+    var short_name: String?
+    var status: String?
+    
+    var chair1: String?
+    var phone1: String?
+    
+    var chair2: String?
+    var phone2: String?
+    
+    var chair3: String?
+    var phone3: String?
+    
+    var chair4: String?
+    var phone4: String?
+    
+    var chair5: String?
+    var phone5: String?
+    
+}
