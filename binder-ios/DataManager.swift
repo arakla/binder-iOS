e924683d33ae7e37e461737c729e0f659775612a
Aamer Rakla <arakla@AAMER-MAC.WV.CC.CMU.EDU>
2015-12-16 13:04:59 -0500

    Outstanding commit; JSON should be working

--- binder-ios/DataManager.swift
+++ binder-ios/DataManager.swift
@@ -22,33 +22,16 @@ class DataManager {
         })
     }
     
-    class func getOrgData() {
-        DataManager.getOrgDataFromFileWithSuccess { (data) -> Void in
-            var json = JSON(data: data)
+    class func getForecastDataFromFileWithSuccess(success: ((data: NSData) -> Void)) {
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), {
+            let filePath = NSBundle.mainBundle().pathForResource("forecast",ofType:"json")
             
-            if let organizations = json["organizations"][0]["name"].string {
-                println("Orgs: \(organizations)")
+            var readError:NSError?
+            if let data = NSData(contentsOfFile:filePath!,
+                options: NSDataReadingOptions.DataReadingUncached,
+                error:&readError) {
+                    success(data: data)
             }
-            
-//            if let list = json["list"].array {
-//                if let tempK = list[0]["main"]["temp"].double {
-//                    var tempF = (tempK - 273.15) * 1.8000 + 32.00
-//                    println("Temp in K: \(tempK)")
-//                    println("Temp in F: \(tempF)")
-//                }
-//            }
-//            
-//            if let list = json["list"].array {
-//                if let period = list[0]["dt_txt"].string {
-//                    let dateFormatter = NSDateFormatter()
-//                    dateFormatter.dateFormat = ""
-//                    let periodF = dateFormatter.dateFromString(period)
-//                    println("Period: \(periodF)")
-//                }
-//            }
-            
-//            var orgObj = map(organization){Organization(name: $0, ship: $1)}
-            
-        }
+        })
     }
 }
--- binder-ios/OrganizationController.swift
+++ binder-ios/OrganizationController.swift
@@ -18,7 +18,58 @@ class OrganizationController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        getOrgData();
+        println("In Orgcntrl View Did")
+        DataManager.getOrgDataFromFileWithSuccess { (data) -> Void in
+            println("Into OrgDataFromFile...")
+            var parseError: NSError?
+            let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parseError)
+            if parseError != nil {
+                println("Parse Error")
+                return
+            }
+            println("json Parsed")
+            if let jsonDict = parsedObject as? NSDictionary {
+                println(jsonDict)
+//                let orgDict = jsonDict["organizations"] as! NSDictionary
+//                let name = organizations["name"] as? String
+//                println("Optional Binding: \(name)")
+            }
+
+            
+        }
+        
+//        DataManager.getForecastDataFromFileWithSuccess { (data) -> Void in
+//            var parseError: NSError?
+//            let parsedObject: AnyObject? = NSJSONSerialization.JSONObjectWithData(data, options: NSJSONReadingOptions.AllowFragments, error: &parseError)
+//            if parseError != nil {
+//                return
+//            }
+//            
+//            println("\nPart 1\n")
+//            
+//            if let jsonDict = parsedObject as? NSDictionary {
+//                //            println(jsonDict)
+//                let cityDict = jsonDict["city"] as! NSDictionary
+//                let name = cityDict["name"] as? String
+//                println("Optional Binding: \(name)")
+//            }
+//            
+//            println("\nProf H\n")
+//            
+//            // Prof H's answer w/safe unwrap
+//            if let forecastStream = parsedObject as? NSDictionary {
+//                println("forecast opened")
+//                if let city = forecastStream["city"] as? NSDictionary {
+//                    println("city found")
+//                    if let name = city["name"] as? String {
+//                        println("Optional Binding: \(name)")
+//                    }
+//                    else {
+//                        println("failed on name")
+//                    }
+//                }
+//            }
+//        }
     }
     
     override func didReceiveMemoryWarning() {
