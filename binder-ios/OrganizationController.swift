d7cd45d2e8dea79ba0f66ba451570afb1e7865c6
Aamer Rakla <arakla@AAMER-MAC.WV.CC.CMU.EDU>
2015-12-16 19:33:01 -0500

    Org Controller cleaned up (somewhat)

--- binder-ios/OrganizationController.swift
+++ binder-ios/OrganizationController.swift
@@ -17,65 +17,11 @@ class OrganizationController: UITableViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("In ViewDidLoad")
-        loadOrgData()
-//        DataManager.getOrgDataFromFileWithSuccess { (data) -> Void in
-//            var json = JSON(data: data)
-//            
-//            println(json["organizations"].count)
-//            
-//            for var i = 0; i < json["organizations"].count; i++ {
-////                println("in for loop for load data")
-//                self.organizations.append(Organization())
-//                
-//                if let orgName = json["organizations"][i]["name"].string {
-//                    self.organizations[i].name = orgName
-////                    println(orgName)
-//                }
-//                
-//                if let orgShort = json["organizations"][i]["short_name"].string {
-//                    self.organizations[i].short_name = orgShort
-//                }
-//                
-//                if let orgCat = json["organizations"][i]["category"].string {
-//                    self.organizations[i].category = orgCat
-//                }
-//                
-//                if let chairName1 = json["organizations"][i]["booth_chairs"][0]["cached_name"].string {
-//                    self.organizations[i].chair1 = chairName1
-//                }
-//                
-//                if let chairPhone1 = json["organizations"][i]["booth_chairs"][0]["phone_number"].string {
-//                    self.organizations[i].phone1 = chairPhone1
-//                }
-//                
-//                if let chairName2 = json["organizations"][i]["booth_chairs"][1]["cached_name"].string {
-//                    self.organizations[i].chair2 = chairName2
-//                }
-//                
-//                if let chairPhone2 = json["organizations"][i]["booth_chairs"][1]["phone_number"].string {
-//                    self.organizations[i].phone2 = chairPhone2
-//                }
-//                
-//                if let chairName3 = json["organizations"][i]["booth_chairs"][2]["cached_name"].string {
-//                    self.organizations[i].chair3 = chairName3
-//                }
-//                
-//                if let chairPhone3 = json["organizations"][i]["booth_chairs"][2]["phone_number"].string {
-//                    self.organizations[i].phone3 = chairPhone3
-//                }
-//            }
-////            println("Inside DataManager")
-////            for var r = 0; r < self.organizations.count; r++ {
-////                println(self.organizations[r].name!)
-////            }
-//        }
         
-        println("Data Loaded")
+        println("In ViewDidLoad")
+        
+        loadOrgData()
         println("Org Count: \(organizations.count)")
-        for var i = 0; i < organizations.count; i++ {
-            println(organizations[i].name!)
-        }
         println("end view did load")
         // Do any additional setup after loading the view, typically from a nib.
     }
@@ -124,9 +70,7 @@ class OrganizationController: UITableViewController {
     func loadOrgData() {
         DataManager.getOrgDataFromFileWithSuccess { (data) -> Void in
             var json = JSON(data: data)
-            println("Loading Data")
             for var i = 0; i < json["organizations"].count; i++ {
-//                println("in for loop for load data")
                 self.organizations.append(Organization())
 
                 if let orgName = json["organizations"][i]["name"].string {
@@ -165,9 +109,7 @@ class OrganizationController: UITableViewController {
                     self.organizations[i].phone3 = chairPhone3
                 }
             }
-            println("DataMan Fin")
         }
-        println("Load fin")
     }
     
 }
